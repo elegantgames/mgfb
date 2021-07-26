deafc7d725c2a073ac71fbd472bef34fd2af17cc
elegantgames <elegantgames.developer@gmail.com>
2021-07-26 18:40:45 +0430

    upoooolad a nnnew gamee

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1627301981,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/animals1-sheet0.png",
+		"images/animals1-sheet1.png",
+		"images/sprite2-sheet0.png",
+		"images/point-sheet0.png",
+		"images/kashi-sheet0.png",
+		"images/winboard-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/blackwhitebk-sheet0.png",
+		"images/blackwhitebk-sheet1.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"channel.html"
+	]
+}
